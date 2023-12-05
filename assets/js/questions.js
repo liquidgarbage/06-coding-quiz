0807d2f0cbc14bfdc6ff08035ff62663d92d055a
Milo <128057494+liquidgarbage@users.noreply.github.com>
2023-12-05 15:33:48 +0000

    questions added to js file

---
+++ assets/js/questions.js
@@ -0,0 +1,7 @@
+var questionArray = ['Arrays in JavaScript can be used to store ________.',
+'The condition in an if/else statement is enclosed within _________.',
+'String values must be enclosed within ________ when being assigned to variables.',
+'A very useful tool used during development and debugging for printing content to the debugger is:', 
+'Commonly used data types DO NOT include:'];
+
+//add mutiple choice answer to array
