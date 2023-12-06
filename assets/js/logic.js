eb6414ed031ce9ab01da1296dc4d008f1de81bfc
Milo <128057494+liquidgarbage@users.noreply.github.com>
2023-12-06 13:38:05 +0000

    started logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,45 @@
-// start button event listener which srtas timer and renders questions
+// start button event listener which starts timer and renders questions
+// pressing start hides start screen and shows questions
+// timer function which counts down from 60 seconds and ends quiz when it reaches 0
 // second button event listener which checks answers, gives feedback, takes 10 seconds off timer in wrong,  and renders next question
 //end of quiz event listener which renders final score(time) and initials input, saves high scores to local storage
-//
+//high scores button event listener which renders high scores from local storage
+
+// Select the start button, start page, questions, and timer div
+let startButton = document.getElementById('start');
+let startPage = document.getElementById('start-screen');
+let questions = document.getElementById('questions');
+let timerDiv = document.getElementById('.timer');
+
+// Timer function
+let timer = 60;
+let timerId = null;
+
+// Function to start the timer
+function startTimer() {
+  timerId = setInterval(function() {
+    timer--;
+    timerDiv.textContent = timer;
+    console.log(timer);
+    if (timer === 0) {
+      clearInterval(timerId);
+      console.log('Timer has reached 0 and has been stopped.');
+    }
+  }, 1000);
+}
+
+// Function to stop the timer
+function stopTimer() {
+  clearInterval(timerId);
+}
+
+// Add an event listener to the start button
+startButton.addEventListener('click', function() {
+  if (timerId) {
+    stopTimer();
+  } else {
+    startTimer();
+    startPage.style.display = 'hide';
+    questions.style.display = 'block';
+  }
+});
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,7 @@
-var questionArray = ['Arrays in JavaScript can be used to store ________.',
-'The condition in an if/else statement is enclosed within _________.',
-'String values must be enclosed within ________ when being assigned to variables.',
-'A very useful tool used during development and debugging for printing content to the debugger is:', 
-'Commonly used data types DO NOT include:'];
-
-//add mutiple choice answer to array
+var questionArray = [
+['Arrays in JavaScript can be used to store ________.'[ 'numbers and strings', 'other arrays', 'booleans', 'all of the above']],
+['The condition in an if/else statement is enclosed within _________.',[ 'quotes', 'curly brackets', 'parentheses', 'square brackets']],
+['String values must be enclosed within ________ when being assigned to variables.',   [ 'commas', 'curly brackets', 'quotes', 'parentheses']],
+['A very useful tool used during development and debugging for printing content to the debugger is:',[ 'JavaScript', 'terminal/bash', 'for loops', 'console.log']],
+['Commonly used data types DO NOT include:'[ 'strings', 'booleans', 'alerts', 'numbers']],
+]
